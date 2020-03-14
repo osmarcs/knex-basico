df689deb7e2be79bdf602e3748d0d6073ed89a1a
Osmar <osmar.costa.s@gmail.com>
2020-03-14 15:32:39 -0300

    add dependencia dotenv, add DB_HOST

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,11 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   client: 'mysql',
   connection: {
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user:     process.env.DB_USER,
     password: process.env.DB_PASSWORD,
