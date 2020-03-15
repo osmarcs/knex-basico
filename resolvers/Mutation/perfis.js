0e3b0764f184e859fdfdb82d50c7b2ce39cf8533
Osmar <osmar.costa.s@gmail.com>
2020-03-15 00:29:40 -0300

    criado mutation de perfil

--- resolvers/Mutation/perfis.js
+++ resolvers/Mutation/perfis.js
@@ -1,11 +1,40 @@
+const db = require('../../config/database');
+const { perfil: obterPerfil } = require('../Query/perfis');
+
 module.exports = {
-  adicionarPerfil(_, { dados }) {
-
+  async adicionarPerfil(_, { dados }) {
+    try {
+      const [ id ] = await db('perfis').insert(dados);
+      return db('perfis').where({ id }).first();
+    } catch (error) {
+      console.log(error)
+    }
   },
-  alterarPerfil(_, { filtro, dados }) {
+  async alterarPerfil(_, { filtro, dados }) {
+    const perfil = await obterPerfil(_, { filtro });
+    if (perfil) {
+      return null;
+    }
 
+    try {
+      await db('perfis').where({ id: perfil.id }).update(dados);
+      return {...perfil, ...dados};
+    } catch (error) {
+      console.log(error)
+    }
   },
-  deletarPerfil(_, { filtro }) {
+  async deletarPerfil(_, { filtro }) {
+    const perfil = await obterPerfil(_, { filtro });
+    if (perfil) {
+      return null;
+    }
 
+    try {
+      await db('usuarios_perfis').where({ perfil_id: perfil.id }).delete();
+      await db('perfis').where({ id: perfil.id }).delete();
+      return perfil;
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
